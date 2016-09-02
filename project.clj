976c6245c6597da85152a2c46d301b9bd83c0f31
Jones Melton <shavah@gmail.com>
2016-09-02 14:04:14 -0500

    Set up deps and first test

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [twitter-api "0.7.8"]
+                 [environ "1.1.0"]
+                 [clj-http "2.1.0"]]
   :main ^:skip-aot contest-winner.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
+                                  [lein-kibit "0.1.2"]
+                                  [venantius/ultra "0.4.1"]])
--- test/contest_winner/core_test.clj
+++ test/contest_winner/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [contest-winner.core :refer :all]))
 
-(deftest a-test
+(deftest CI
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0))))
