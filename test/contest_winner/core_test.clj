49bfa5e9e1d0121df6c7884756686e0ae88600b9
Jones Melton <shavah@gmail.com>
2016-09-02 13:52:14 -0500

    Initial commit

---
+++ test/contest_winner/core_test.clj
@@ -0,0 +1,7 @@
+(ns contest-winner.core-test
+  (:require [clojure.test :refer :all]
+            [contest-winner.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
